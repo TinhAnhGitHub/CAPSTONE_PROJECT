75529933e52290335860e26f0f292fd0ae26a2ce
TinhAnhGitHub <anh.nguyennhu2306@hcmut.edu.vn>
2026-03-15 21:28:50 +0700

    Add: add sparse embedding server

---
+++ inference/triton_infra/model_repository/splade/1/model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import triton_python_backend_utils as pb_utils
+from fastembed import SparseTextEmbedding
+
+MODEL_NAME = "prithivida/Splade_PP_en_v1"
+
+class TritonPythonModel:
+
+    def initialize(self, args):
+        self.model = SparseTextEmbedding(model_name=MODEL_NAME)
+
+    def execute(self, requests):
+        responses = []
+
+        for request in requests:
+            input_tensor = pb_utils.get_input_tensor_by_name(request, "TEXT")
+            texts = input_tensor.as_numpy().astype(str).flatten().tolist()
+
+            embeddings = list(self.model.embed(texts))
+
+            indices_batch = []
+            values_batch = []
+
+            for emb in embeddings:
+                indices_batch.append(np.array(emb.indices, dtype=np.int32))
+                values_batch.append(np.array(emb.values, dtype=np.float32))
+
+            out_indices = pb_utils.Tensor(
+                "INDICES", np.array(indices_batch, dtype=object)
+            )
+
+            out_values = pb_utils.Tensor(
+                "VALUES", np.array(values_batch, dtype=object)
+            )
+
+            responses.append(
+                pb_utils.InferenceResponse(
+                    output_tensors=[out_indices, out_values]
+                )
+            )
+
+        return responses
