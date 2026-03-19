e87585e872ea25cad8f50ed1fbcf806bb74887a5
TinhAnhGitHub <anh.nguyennhu2306@hcmut.edu.vn>
2026-03-19 17:33:48 +0700

    refactor(tasks): remove separate multimodal embedding tasks

--- video_pipeline/src/video_pipeline/task/segment_caption_multimodal_embedding/__init__.py
+++
@@ -1,9 +0,0 @@
-from .main import (
-    segment_caption_multimodal_embedding_chunk_task,
-    SegmentCaptionMultimodalEmbeddingTask,
-)
-
-__all__ = [
-    "segment_caption_multimodal_embedding_chunk_task",
-    "SegmentCaptionMultimodalEmbeddingTask",
-]
